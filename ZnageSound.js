572456ba227c5ea0d1b29b2b003675f47ae47946
pooya.bazzaz <pooya.bazzaz@geeks.ltd.uk>
2017-12-06 16:16:03 +0000

    Fixed the issue of existing "audioCtx" upon restart.

--- ZnageSound.js
+++ ZnageSound.js
@@ -1,26 +1,28 @@
 //#############################  Sound  #############################################
 //###################################################################################
 window.initialiseSound = function(){
-	window.audioCtx = new AudioContext();
-
-	window.foodBeepOscillator = window.audioCtx.createOscillator();
-	window.foodBeepOscillator.frequency.value = 2000;
-	window.foodBeepOscillator.connect(window.audioCtx.destination);
-	window.foodBeepOscillator.start();
-	window.foodBeepOscillator.disconnect();
+	if(typeof window.audioCtx === 'undefined'){
+		window.audioCtx = new AudioContext();
 	
-	window.mouseInBeepOscillator = window.audioCtx.createOscillator();
-	window.mouseInBeepOscillator.frequency.value = 3000;
-	window.mouseInBeepOscillator.connect(window.audioCtx.destination);
-	window.mouseInBeepOscillator.start();
-	window.mouseInBeepOscillator.disconnect();
-	
-	window.mouseOutBeepOscillator = window.audioCtx.createOscillator();
-	window.mouseOutBeepOscillator.frequency.value = 2500;
-	window.mouseOutBeepOscillator.connect(window.audioCtx.destination);
-	window.mouseOutBeepOscillator.start();
-	window.mouseOutBeepOscillator.disconnect();
-}
+		window.foodBeepOscillator = window.audioCtx.createOscillator();
+		window.foodBeepOscillator.frequency.value = 2000;
+		window.foodBeepOscillator.connect(window.audioCtx.destination);
+		window.foodBeepOscillator.start();
+		window.foodBeepOscillator.disconnect();
+		
+		window.mouseInBeepOscillator = window.audioCtx.createOscillator();
+		window.mouseInBeepOscillator.frequency.value = 3000;
+		window.mouseInBeepOscillator.connect(window.audioCtx.destination);
+		window.mouseInBeepOscillator.start();
+		window.mouseInBeepOscillator.disconnect();
+		
+		window.mouseOutBeepOscillator = window.audioCtx.createOscillator();
+		window.mouseOutBeepOscillator.frequency.value = 2500;
+		window.mouseOutBeepOscillator.connect(window.audioCtx.destination);
+		window.mouseOutBeepOscillator.start();
+		window.mouseOutBeepOscillator.disconnect();
+	};
+};
 window.foodBeep = function(){
 	window.foodBeepOscillator.connect(window.audioCtx.destination);
 	setTimeout(function(){window.foodBeepOscillator.disconnect()},70);
