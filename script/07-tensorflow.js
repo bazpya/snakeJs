e5d3963445d42e40b8d8eedfd620eea21250eb57
pyabzz <>
2020-11-09 23:58:39 +0000

    Add visor

--- script/07-tensorflow.js
+++ script/07-tensorflow.js
@@ -6,6 +6,18 @@ Ai = function (game) {
     log(tf.version);
     log("tf backend:");
     log(tf.getBackend());
-    
     tf.add(1, 3).print();
+    tfvis.visor();
+    const inputMatrix = {
+        values: [
+            [1,1,1,1,1],
+            [1,0,0,0,1],
+            [1,0,0,0,1],
+            [1,0,0,0,1],
+            [1,0,0,0,1],
+            [1,1,1,1,1]
+        ]
+    };
+    const surface = { name: "Dasoo heatmap", tab: "Dasoo charts" }
+    tfvis.render.heatmap(surface, inputMatrix);
 }
