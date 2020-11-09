69d8e0341e668618e3728361cdc3e691708cf6cb
pyabzz <>
2020-11-09 02:29:17 +0000

    Refactor sound definitions

--- 05-sound.js
+++ 05-sound.js
@@ -1,42 +1,27 @@
+soundDefinitions = Object.freeze({
+    "foodBeep": [2000, 70],
+    "mouseInBeep": [3000, 50],
+    "mouseOutBeep": [2500, 50]
+});
+
 Sound = function (volume) {
     this.audioCtx = new AudioContext();
     this.gainNode = this.audioCtx.createGain();
     this.gainNode.connect(this.audioCtx.destination);
     this.gainNode.gain.value = volume;
 
-    this.foodBeepOscillator = this.audioCtx.createOscillator();
-    this.foodBeepOscillator.frequency.value = 2000;
-    this.foodBeepOscillator.connect(this.gainNode);
-    this.foodBeepOscillator.start();
-    this.foodBeepOscillator.disconnect();
+    for (let key in soundDefinitions) {
+        let oscillatorName = key + "Oscillator";
+        this[oscillatorName] = this.audioCtx.createOscillator();
+        this[oscillatorName].frequency.value = soundDefinitions[key][0];
+        this[oscillatorName].connect(this.gainNode);
+        this[oscillatorName].start();
+        this[oscillatorName].disconnect();
 
-    this.mouseInBeepOscillator = this.audioCtx.createOscillator();
-    this.mouseInBeepOscillator.frequency.value = 3000;
-    this.mouseInBeepOscillator.connect(this.gainNode);
-    this.mouseInBeepOscillator.start();
-    this.mouseInBeepOscillator.disconnect();
-
-    this.mouseOutBeepOscillator = this.audioCtx.createOscillator();
-    this.mouseOutBeepOscillator.frequency.value = 2500;
-    this.mouseOutBeepOscillator.connect(this.gainNode);
-    this.mouseOutBeepOscillator.start();
-    this.mouseOutBeepOscillator.disconnect();
-}
-
-Sound.prototype.foodBeep = function () {
-    let me = this;
-    this.foodBeepOscillator.connect(this.gainNode);
-    setTimeout(() => { me.foodBeepOscillator.disconnect() }, 70);
-}
-
-Sound.prototype.mouseInBeep = function () {
-    let me = this;
-    this.mouseInBeepOscillator.connect(this.gainNode);
-    setTimeout(() => { me.mouseInBeepOscillator.disconnect() }, 50);
-}
-
-Sound.prototype.mouseOutBeep = function () {
-    let me = this;
-    this.mouseOutBeepOscillator.connect(this.gainNode);
-    setTimeout(() => { me.mouseOutBeepOscillator.disconnect() }, 50);
+        this[key] = function () {
+            let me = this;
+            this[oscillatorName].connect(this.gainNode);
+            setTimeout(() => { me[oscillatorName].disconnect() }, soundDefinitions[key][1]);
+        }
+    }
 }
