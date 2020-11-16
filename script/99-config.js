59093f16d0e36cc236feb7d1e0d013eeef4c6524
pyabzz <>
2020-11-16 11:52:45 +0000

    Add development tasks

--- script/99-config.js
+++ script/99-config.js
@@ -18,3 +18,11 @@ znakeConfig = {
     soundVolume: 0.15,  // [0~1]
     runMode: runModeEnum.manual,  // 0 = manual 1 = auto
 }
+
+//Todo: Support a wide range of grid sizes
+//Todo: Add option to prevent worm growth
+//Todo: Add option to only drop food upon initialisation based on a given pattern
+//Todo: Experiment with model save, load, etc.
+//Todo: Experiment with model history, crossover, genetic algorithm, etc.
+//Todo: Use the tidy() function for model operations
+//Todo: After implementing genetic algorithm, considre adding other criterial like life time of the worm
